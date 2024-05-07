2c1b041ed6edd0bf9d51620c694ea454efc1f695
ndsurgenor <ndsurgenor@gmail.com>
2024-05-07 23:49:37 +0100

    Add code to loop through posts

--- index.php
+++ index.php
@@ -18,26 +18,49 @@ include "includes/header.php";
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
-                <h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
+                <?php
 
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+                $queryTable = "SELECT * FROM posts";
+                $queryPosts = mysqli_query($connection, $queryTable);
 
-                <hr>
+                while ($row = mysqli_fetch_assoc($queryPosts)) {
+                    $postTitle = $row["post_title"];
+                    $postAuthor = $row["post_author"];
+                    $postDate = $row["post_date"];
+                    $postImage = $row["post_image"];
+                    $postContent = $row["post_content"];
+
+                ?>
+
+                    <h1 class="page-header">
+                        Page Heading
+                        <small>Secondary Text</small>
+                    </h1>
+
+                    <!-- First Blog Post -->
+                    <h2>
+                        <a href="#"><?php ?></a>
+                    </h2>
+                    <p class="lead">
+                        by <a href="index.php">Start Bootstrap</a>
+                    </p>
+                    <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                    <hr>
+                    <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+                    <hr>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                    <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                    <hr>
+
+                <?php
+
+                    echo "<li><a href='#'>{$postTitle}</li>";
+                }
+
+                ?>
+
+
 
             </div>
 

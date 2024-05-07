23b897f5763d7ce4cab845975d41100033ee0169
ndsurgenor <ndsurgenor@gmail.com>
2024-05-07 23:54:07 +0100

    Add variables to create post titles and content

--- index.php
+++ index.php
@@ -39,28 +39,21 @@ include "includes/header.php";
 
                     <!-- First Blog Post -->
                     <h2>
-                        <a href="#"><?php ?></a>
+                        <a href="#"><?php echo $postTitle ?></a>
                     </h2>
                     <p class="lead">
-                        by <a href="index.php">Start Bootstrap</a>
+                        by <a href="index.php"><?php echo $postAuthor ?></a>
                     </p>
-                    <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
+                    <p><span class="glyphicon glyphicon-time"></span> <?php echo $postDate ?></p>
                     <hr>
                     <img class="img-responsive" src="http://placehold.it/900x300" alt="">
                     <hr>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
+                    <p><?php echo $postContent ?></p>
                     <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                     <hr>
 
-                <?php
-
-                    echo "<li><a href='#'>{$postTitle}</li>";
-                }
-
-                ?>
-
-
+                <?php } ?>
 
             </div>
 

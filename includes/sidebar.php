fef3eb0c89a08ad73b1ee70a1c1f875e7a5001ba
ndsurgenor <ndsurgenor@gmail.com>
2024-05-09 23:57:14 +0100

    Move sidebar.php to main directory

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,25 +1,9 @@
-<div class="col-md-4">
-
-    <?php
-
-    if (isset($_POST["submit"])) {
-
-        $search = $_POST["search"];
-        $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
-        $searchQuery = mysqli_query($connection, $query);
-
-        if (!$searchQuery) {
-
-            die("QUERY FAILED" . mysqli_error($connection));
-        }
-    }
-
-    ?>
+<div class="col-md-4">    
 
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
-        <form action="" method="post">
+        <form action="search.php" method="post">
             <div class="input-group">
                 <input name="search" type="text" class="form-control">
                 <span class="input-group-btn">
---
+++ search.php
@@ -0,0 +1,92 @@
+<?php
+
+include "includes/db.php";
+include "includes/header.php";
+
+?>
+
+<body>
+
+    <!-- Navigation -->
+    <?php include "includes/nav.php"; ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                <?php
+
+                if (isset($_POST["submit"])) {
+
+                    $search = $_POST["search"];
+                    $query = "SELECT * FROM posts WHERE post_tags LIKE '%$search%'";
+                    $searchQuery = mysqli_query($connection, $query);
+
+                    if (!$searchQuery) {
+
+                        die("QUERY FAILED" . mysqli_error($connection));
+                    } else {
+
+                        $queryTable = "SELECT * FROM posts";
+                        $queryPosts = mysqli_query($connection, $queryTable);
+
+                        while ($row = mysqli_fetch_assoc($queryPosts)) {
+                            $postTitle = $row["post_title"];
+                            $postAuthor = $row["post_author"];
+                            $postDate = $row["post_date"];
+                            $postImage = $row["post_image"];
+                            $postContent = $row["post_content"];
+                        }
+                ?>
+                        <h1 class="page-header">
+                            Page Heading
+                            <small>Secondary Text</small>
+                        </h1>
+
+                        <!-- First Blog Post -->
+                        <h2>
+                            <a href="#"><?php echo $postTitle ?></a>
+                        </h2>
+                        <p class="lead">
+                            by <a href="index.php"><?php echo $postAuthor ?></a>
+                        </p>
+                        <p><span class="glyphicon glyphicon-time"></span> <?php echo $postDate ?></p>
+                        <hr>
+                        <img class="img-responsive" src="images/<?php echo $postImage ?>" alt="">
+                        <hr>
+                        <p><?php echo $postContent ?></p>
+                        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                        <hr>
+                <?php
+                    }
+                }
+                ?>
+
+            </div>
+
+            <!-- Blog Sidebar Widgets Column -->
+            <?php include "includes/sidebar.php"; ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <!-- Footer -->
+        <?php include "includes/footer.php"; ?>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>

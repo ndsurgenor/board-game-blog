7b5ead86a44b67992337a990209f9c50a2bcaa06
ndsurgenor <ndsurgenor@gmail.com>
2024-05-10 22:08:41 +0100

    Add code for widget.php

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -30,7 +30,7 @@
 
         <h4>Blog Categories</h4>
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
 
                     <?php
@@ -44,20 +44,6 @@
 
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
     </div>

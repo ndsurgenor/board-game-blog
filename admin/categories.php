c09bd294799b7511e9a19a3b87301c2f99c3a843
ndsurgenor <ndsurgenor@gmail.com>
2024-05-19 22:57:49 +0100

    Add code to allow adding of categories in admin

--- admin/categories.php
+++ admin/categories.php
@@ -25,7 +25,29 @@
                         </h1>
 
                         <div class="col-xs-6">
-                            <form action="">
+
+                            <?php
+
+                            if (isset($_POST["submit"])) {
+                                $cat_title = $_POST["cat_title"];
+
+                                if ($cat_title == "" || empty($cat_title)) {
+                                    echo "This field cannot be left blank";
+                                } else {
+                                    $query = "INSERT INTO category(cat_title)";
+                                    $query .= "VALUE ('{$cat_title}')";
+
+                                    $createCategory = mysqli_query($connection, $query);
+
+                                    if (!$createCategory) {
+                                        die("QUERY FAILED" . mysqli_error($connection));
+                                    }
+                                }
+                            }
+
+                            ?>
+
+                            <form action="" method="post">
                                 <div class="form-group">
                                     <input class="form-control" type="text" name="cat_title">
                                 </div>

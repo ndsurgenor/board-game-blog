8f71fe150fcf25ca11dc8ea7d692687fee3bffa5
ndsurgenor <ndsurgenor@gmail.com>
2024-05-29 23:08:20 +0100

    Add code to send data to database

--- admin/includes/add-post.php
+++ admin/includes/add-post.php
@@ -6,7 +6,7 @@ if (isset($_POST["submit"])) {
     $post_author = $_POST["post_author"];
     $post_date = date("d-m-y");
     $post_image = $_FILES["post_image"]["name"];
-    $post_image_temp = $_FILES["post_image"]["temp_name"];
+    $post_image_temp = $_FILES["post_image"]["tmp_name"];
     $post_content = $_POST["post_content"];
     $post_tags = $_POST["post_tags"];
     $post_comment_count = 4;
@@ -37,6 +37,12 @@ if (isset($_POST["submit"])) {
         '{$post_comment_count}',
         '{$post_status}'
         )";
+
+    $createPost = mysqli_query($connection, $query);
+
+    if (!$createPost) {
+        die("QUERY FAILED" . mysqli_error($connection));
+    };
 }
 
 ?>
@@ -45,38 +51,31 @@ if (isset($_POST["submit"])) {
 
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input type="text" class="form-control" name="title">
+        <input type="text" class="form-control" name="post_title">
     </div>
 
     <div class="form-group">
-        <label for="category">Category</label>
-        <select name="post_category_id" id=""></select>
+        <label for="category">Post Category ID</label>
+        <input type="text" class="form-control" name="post_category_id" id=""></select>
 
     </div>
 
-
     <div class="form-group">
-        <label for="users">Users</label>
-        <select name="post_user" id=""></select>
-
+        <label for="title">Post Author</label>
+        <input type="text" class="form-control" name="post_author">
     </div>
 
-    <!-- <div class="form-group">
-         <label for="title">Post Author</label>
-          <input type="text" class="form-control" name="author">
-      </div> -->
-
     <div class="form-group">
+        <label for="post_status">Post Status</label>
         <select name="post_status" id="">
-            <option value="draft">Post Status</option>
-            <option value="published">Published</option>
             <option value="draft">Draft</option>
+            <option value="published">Published</option>
         </select>
     </div>
 
     <div class="form-group">
         <label for="post_image">Post Image</label>
-        <input type="file" name="image">
+        <input type="file" name="post_image">
     </div>
 
     <div class="form-group">
@@ -86,7 +85,7 @@ if (isset($_POST["submit"])) {
 
     <div class="form-group">
         <label for="post_content">Post Content</label>
-        <textarea class="form-control " name="post_content" id="" cols="30" rows="10">
+        <textarea class="form-control" name="post_content" id="" cols="30" rows="10">
          </textarea>
     </div>
 
